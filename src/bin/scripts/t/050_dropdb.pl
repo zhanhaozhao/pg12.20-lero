120488c7892e27c439ea698abb0e0280dfc31580
zhanhaozhao <zhanhao@nus.edu.sg>
2025-05-07 06:35:07 +0000

    initial commit 12.20

---
+++ src/bin/scripts/t/050_dropdb.pl
@@ -0,0 +1,32 @@
+use strict;
+use warnings;
+
+use PostgresNode;
+use TestLib;
+use Test::More tests => 12;
+
+program_help_ok('dropdb');
+program_version_ok('dropdb');
+program_options_handling_ok('dropdb');
+
+my $node = get_new_node('main');
+$node->init;
+$node->start;
+
+$node->safe_psql('postgres', 'CREATE DATABASE foobar1');
+$node->issues_sql_like(
+	[ 'dropdb', 'foobar1' ],
+	qr/statement: DROP DATABASE foobar1/,
+	'SQL DROP DATABASE run');
+
+$node->command_fails([ 'dropdb', 'nonexistent' ],
+	'fails with nonexistent database');
+
+# check that invalid database can be dropped with dropdb
+$node->safe_psql(
+	'postgres', q(
+	CREATE DATABASE regression_invalid;
+	UPDATE pg_database SET datconnlimit = -2 WHERE datname = 'regression_invalid';
+));
+$node->command_ok([ 'dropdb', 'regression_invalid' ],
+  'invalid database can be dropped');
